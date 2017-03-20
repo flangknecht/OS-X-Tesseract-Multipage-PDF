1c3dcd0f2eb24be8b6a300573c19b90d4d471bfd
Florian Langknecht <flangknecht@united-internet.de>
2017-03-20 12:27:35 +0100

    Minor changes

--- recognize_pdf.sh
+++ recognize_pdf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-JOINSCRIPT='/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py'
 RESOLUTION=300
 LANG="deu"
 
@@ -16,18 +15,19 @@ FINALNAME="$OUTPUTFILE.pdf"
 TEMPDIR="temp_page_dir"
 OUTPUTDIR="$TEMPDIR/OCR"
 
-mkdir -p $TEMPDIR
-mkdir -p $OUTPUTDIR
+mkdir -p "$TEMPDIR"
+mkdir -p "$OUTPUTDIR"
 
 for i in $(seq 1 $PAGES); do
     echo "Converting page $i/$PAGES to image."
-    convert -density $RESOLUTION -set units PixelsPerInch -depth 8 "$SOURCEFILE"\[$(($i - 1 ))\] $TEMPDIR/page$i.png
+    convert -density "$RESOLUTION"x"$RESOLUTION" -set units PixelsPerInch -depth 8 "$SOURCEFILE"\[$(($i - 1 ))\] $TEMPDIR/page$i.png
     echo "Running OCR for languages $LANG."
-    tesseract $TEMPDIR/page$i.png $OUTPUTDIR/"$OUTPUTFILE"$i -l "$LANG" pdf
+    tesseract "$TEMPDIR/page"$i.png "$OUTPUTDIR"/"$OUTPUTFILE"$i -l "$LANG" pdf
     
     echo "---"
 done
 
 echo "Combining searchable PDFs into one file..."
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="$OUTPUTFILE"_ocr.pdf "$OUTPUTDIR/$OUTPUTFILE"*.pdf && rm -r $OUTPUTDIR && rm -r $TEMPDIR
-echo "All done. Output file name is :$OUTPUTFILE"_ocr.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="$OUTPUTFILE ocr.pdf" "$OUTPUTDIR/$OUTPUTFILE"*.pdf && rm -r "$OUTPUTDIR" && rm -r "$TEMPDIR"
+echo "All done. Output file name is: $OUTPUTFILE ocr.pdf"
+# echo "ALERT:Conversion completed|Output file name is: $OUTPUTFILE ocr.pdf\n" # Displays alert in Platypus app runtime
